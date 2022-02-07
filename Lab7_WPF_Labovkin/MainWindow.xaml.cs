706ff959ec465897e28aa9dd0d075d0b5b4798b5
labov <labov@LAPTOP-RLT6CAF3>
2022-02-07 21:49:30 +0300

    Добавьте файлы проекта.

---
+++ Lab7_WPF_Labovkin/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab7_WPF_Labovkin
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Документ открыт");
+        }
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Документ сохранен");
+        }
+        private void ExitCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            //если textbox пустой окно закроется, если нет не закроется
+
+            if (textBox!=null && textBox.Text.Length == 0)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //метод обработки события, при выборе стиля текста
+        //        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //        {
+        //            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+        //            if (textBox != null)
+        //            {
+        //                textBox.FontFamily = new FontFamily(fontName);
+        //            }
+        //        }
+        //        //метод обработки события, при выборе размера текста
+        //        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        //        {
+        //            if (textBox != null)
+        //            {
+        //                double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+        //                textBox.FontSize = fontSize;
+        //            }
+        //        }
+        //        //метод обработки события, нажатия на кнопку жирности
+        //        private void Button_Click(object sender, RoutedEventArgs e)
+        //        {
+        //            if (textBox.FontWeight == FontWeights.Normal)
+        //            {
+        //                textBox.FontWeight = FontWeights.ExtraBold;
+        //            }
+        //            else
+        //            {
+        //                textBox.FontWeight = FontWeights.Normal;
+        //            }
+        //        }
+        //        //метод обработки события, нажатия на кнопку курсив
+        //        private void Button_Click_1(object sender, RoutedEventArgs e)
+        //        {
+        //            if (textBox.FontStyle == FontStyles.Normal)
+        //            {
+        //                textBox.FontStyle = FontStyles.Italic;
+        //            }
+        //            else
+        //            {
+        //                textBox.FontStyle = FontStyles.Normal;
+        //            }
+        //        }
+        //        //метод обработки события, нажатия на кнопку подчеркивания
+        //        private void Button_Click_2(object sender, RoutedEventArgs e)
+        //        {
+        //            if (textBox.TextDecorations.Count == 0)
+        //            {
+        //                textBox.TextDecorations.Add(TextDecorations.Underline);
+        //            }
+        //            else
+        //            {
+        //                textBox.TextDecorations.RemoveAt(0);
+        //            }
+        //        }
+        //        //метод обработки события, при выборе цвета текста(черный)
+        //        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        //        {
+        //            if (textBox != null)
+        //            {
+        //                textBox.Foreground = Brushes.Black;
+        //            }
+        //        }
+        //        //метод обработки события, при выборе цвета текста(красный)
+        //        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        //        {
+        //            if (textBox != null)
+        //            {
+        //                textBox.Foreground = Brushes.Red;
+        //            }
+        //        }
+
+        //        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        //        {
+        //            OpenFileDialog openFileDialog = new OpenFileDialog();
+        //            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+        //            if (openFileDialog.ShowDialog() == true)
+        //            {
+        //                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+        //            }
+        //        }
+
+        //        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        //        {
+        //            SaveFileDialog saveFileDialog = new SaveFileDialog();
+        //            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+        //            if (saveFileDialog.ShowDialog() == true)
+        //            {
+        //                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+        //            }
+        //        }
+
+        //        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        //        {
+        //            Application.Current.Shutdown();
+        //        }
+        //    }
+    }
+}
---
+++ Lab7_WPF_Labovkin/MyCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab7_WPF_Labovkin
+{
+    class MyCommands
+    {
+        public static RoutedUICommand Exit { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+t"));
+            Exit = new RoutedUICommand("Закрыть","Exit",typeof(MyCommands),inputs);
+        }
+    }
+}
