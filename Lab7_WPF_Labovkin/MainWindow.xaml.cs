254d61a27887f53f8e45aa82f0afa976d8f6cbb7
Дмитрий Лабовкин <labovkindi@mail.ru>
2022-02-08 23:59:05 +0300

    Добвлен словарь ресурсов

--- Lab7_WPF_Labovkin/MainWindow.xaml.cs
+++ Lab7_WPF_Labovkin/MainWindow.xaml.cs
@@ -26,25 +26,25 @@ namespace Lab7_WPF_Labovkin
         {
             InitializeComponent();
         }
-        
+
         private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
-        {
-            MessageBox.Show("Документ открыт");
-        }
-        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
-        {
-            MessageBox.Show("Документ сохранен");
-        }
+        //private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        //{
+        //    MessageBox.Show("Документ открыт");
+        //}
+        //private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        //{
+        //    MessageBox.Show("Документ сохранен");
+        //}
         private void ExitCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             //если textbox пустой окно закроется, если нет не закроется
 
-            if (textBox!=null && textBox.Text.Length == 0)
+            if (textBox != null && textBox.Text.Length == 0)
             {
                 e.CanExecute = true;
             }
@@ -54,119 +54,101 @@ namespace Lab7_WPF_Labovkin
             }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         //метод обработки события, при выборе стиля текста
-        //        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //        {
-        //            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
-        //            if (textBox != null)
-        //            {
-        //                textBox.FontFamily = new FontFamily(fontName);
-        //            }
-        //        }
-        //        //метод обработки события, при выборе размера текста
-        //        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        //        {
-        //            if (textBox != null)
-        //            {
-        //                double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
-        //                textBox.FontSize = fontSize;
-        //            }
-        //        }
-        //        //метод обработки события, нажатия на кнопку жирности
-        //        private void Button_Click(object sender, RoutedEventArgs e)
-        //        {
-        //            if (textBox.FontWeight == FontWeights.Normal)
-        //            {
-        //                textBox.FontWeight = FontWeights.ExtraBold;
-        //            }
-        //            else
-        //            {
-        //                textBox.FontWeight = FontWeights.Normal;
-        //            }
-        //        }
-        //        //метод обработки события, нажатия на кнопку курсив
-        //        private void Button_Click_1(object sender, RoutedEventArgs e)
-        //        {
-        //            if (textBox.FontStyle == FontStyles.Normal)
-        //            {
-        //                textBox.FontStyle = FontStyles.Italic;
-        //            }
-        //            else
-        //            {
-        //                textBox.FontStyle = FontStyles.Normal;
-        //            }
-        //        }
-        //        //метод обработки события, нажатия на кнопку подчеркивания
-        //        private void Button_Click_2(object sender, RoutedEventArgs e)
-        //        {
-        //            if (textBox.TextDecorations.Count == 0)
-        //            {
-        //                textBox.TextDecorations.Add(TextDecorations.Underline);
-        //            }
-        //            else
-        //            {
-        //                textBox.TextDecorations.RemoveAt(0);
-        //            }
-        //        }
-        //        //метод обработки события, при выборе цвета текста(черный)
-        //        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        //        {
-        //            if (textBox != null)
-        //            {
-        //                textBox.Foreground = Brushes.Black;
-        //            }
-        //        }
-        //        //метод обработки события, при выборе цвета текста(красный)
-        //        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
-        //        {
-        //            if (textBox != null)
-        //            {
-        //                textBox.Foreground = Brushes.Red;
-        //            }
-        //        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as string);
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+        //метод обработки события, при выборе размера текста
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble((sender as ComboBox).SelectedItem);
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSize;
+            }
+        }
+        //метод обработки события, нажатия на кнопку жирности
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.ExtraBold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+        //метод обработки события, нажатия на кнопку курсив
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+        //метод обработки события, нажатия на кнопку подчеркивания
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations.Count == 0)
+            {
+                textBox.TextDecorations.Add(TextDecorations.Underline);
+            }
+            else
+            {
+                textBox.TextDecorations.RemoveAt(0);
+            }
+        }
+        //метод обработки события, при выборе цвета текста(черный)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+        //метод обработки события, при выборе цвета текста(красный)
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
 
-        //        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        //        {
-        //            OpenFileDialog openFileDialog = new OpenFileDialog();
-        //            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
-        //            if (openFileDialog.ShowDialog() == true)
-        //            {
-        //                textBox.Text = File.ReadAllText(openFileDialog.FileName);
-        //            }
-        //        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
 
-        //        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
-        //        {
-        //            SaveFileDialog saveFileDialog = new SaveFileDialog();
-        //            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
-        //            if (saveFileDialog.ShowDialog() == true)
-        //            {
-        //                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
-        //            }
-        //        }
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
 
-        //        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
-        //        {
-        //            Application.Current.Shutdown();
-        //        }
-        //    }
+        //private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        //{
+        //    Application.Current.Shutdown();
+        //}
     }
 }
+
